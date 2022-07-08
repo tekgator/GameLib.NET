d303778fbc6dd20b7aceacaa93d70d0c5a41bb03
tekgator <info@tekgator.com>
2022-07-08 12:30:16 +1000

    Add

---
+++ GameLib/Util/ProcessUtil.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace GameLib.Util;
+
+public static class ProcessUtil
+{
+    /// <summary>
+    /// Check whether a executable is running
+    /// </summary>
+    public static bool IsProcessRunning(string? executable) =>
+        !string.IsNullOrEmpty(executable) &&
+        Process.GetProcessesByName(Path.GetFileNameWithoutExtension(executable)).Where(p => !p.HasExited).Any();
+
+
+    /// <summary>
+    /// Forcefully stops a process
+    /// </summary>
+    public static void StopProcess(string? executable)
+    {
+        if (string.IsNullOrEmpty(executable))
+            return;
+
+        foreach (var runningProcess in Process.GetProcessesByName(Path.GetFileNameWithoutExtension(executable)).Where(p => !p.HasExited))
+        {
+            runningProcess.Kill(true);
+        }
+    }
+
+
+}
