081304381ae438e8a4770627bbb9a8d2718f8adb
tekgator <info@tekgator.com>
2022-08-21 20:12:21 +1000

    Move NavigationBar to Views

---
+++ GameLib.Demo/GameLib.Demo.Wpf/Views/NavigationBarView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace GameLib.Demo.Wpf.Views;
+
+public partial class NavigationBarView : UserControl
+{
+    public NavigationBarView()
+    {
+        InitializeComponent();
+    }
+}
