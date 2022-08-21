ec5a8c4acb881a4fb6429c69034c0ad4651ec04e
tekgator <info@tekgator.com>
2022-08-21 13:53:11 +1000

    Setup WPF demo project

---
+++ GameLib.Demo/GameLib.Demo.Wpf/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using GameLib.Demo.Wpf.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace GameLib.Demo.Wpf;
+
+public partial class App : Application
+{
+    private readonly IHost _host;
+
+    public App()
+    {
+        _host = Host
+            .CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton<MainViewModel>();
+
+                services.AddSingleton(s => new MainWindow()
+                {
+                    DataContext = s.GetRequiredService<MainViewModel>()
+                });
+            })
+            .Build();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+
+        MainWindow = _host.Services.GetRequiredService<MainWindow>();
+        MainWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _host.Dispose();
+        base.OnExit(e);
+    }
+
+}
